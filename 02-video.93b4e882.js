282471a7bfb309a32a8f2ff76bc40df9c320638e
VsevolodZharkov <94003560+VsevolodZharkov@users.noreply.github.com>
2022-06-20 15:19:11 +0000

    Deploying to gh-pages from @ VsevolodZharkov/goit-js-hw-08@eb2d92ed1536152f429de938c056af715441a773 🚀

--- 02-video.93b4e882.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("timeupdate",(function(e){var t=e.seconds;localStorage.setItem("videoplayer-current-time",t)}));var o=localStorage.getItem("videoplayer-current-time");console.log(o),t.setCurrentTime(o).then((function(e){}))}();
-//# sourceMappingURL=02-video.93b4e882.js.map
